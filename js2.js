b6cc0516b3c194b6c9dd95ff81151f0d772aebe4
Laastrog <72933540+IceFoxsGacha@users.noreply.github.com>
2022-11-03 17:53:37 +0300

    Add files via upload

--- js2.js
+++ js2.js
@@ -2,19 +2,11 @@ const fetch = require('axios')
 const cheerio = require('cherio');
 const { text } = require('express');
 
-const url = 'https://www.ozon.ru/category/kompyutery-i-komplektuyushchie-15690/';
+;(async () =>{
 
-async function start(){
+
+    const url = 'https://www.ozon.ru/category/kompyutery-i-komplektuyushchie-15690/';
 
     const result = await axios.get(url)
-
-    const $ = await cheerio.load(result.data)
-    $('.kt4').each((_,e) => {
-
-    console.log(text)
-
-    })
-
-    // console.log(result)
-}
-start()
+    console.log(res.data)
+})
